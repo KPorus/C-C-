b4b80496acee658d5266d62567431fa2cc48f074
KPorus <designerfardin2021@gmail.com>
2022-12-25 20:25:39 +0600

    linked list

--- linklistData.cpp
+++ linklistData.cpp
@@ -2,9 +2,9 @@
 
 class Node
 {
-    public:
+public:
     int data;
-    Node * next;
+    Node *next;
 
     Node(int value)
     {
@@ -13,61 +13,87 @@ class Node
     }
 };
 
-void insertValue(Node * &head, int value)
+void insertAtHeadValue(Node *&head, int value)
 {
-    Node * n = new Node(value);
+    Node *n = new Node(value);
+    n->next = head;
+    head = n;
+}
 
-    if(head == NULL)
+void insertAtTailValue(Node *&head, int value)
+{
+    Node *n = new Node(value);
+
+    if (head == NULL)
     {
         head = n;
         return;
     }
 
-    Node * temp = head;
-    while (temp->next !=NULL)
+    Node *temp = head;
+    while (temp->next != NULL)
     {
         temp = temp->next;
     }
     temp->next = n;
-       
 }
 
-void display(Node * head)
+void display(Node *head)
 {
-    Node * temp = head;
-    while (temp!=NULL)
+    Node *temp = head;
+    while (temp != NULL)
     {
-        std::cout<<temp->data<<" ";
+        std::cout << temp->data << " ";
         temp = temp->next;
     }
-    std::cout<<"\n";
+    std::cout << "\n";
 }
 
 int main()
 {
 
-    Node * head = NULL;
+    Node *head = NULL;
     int k;
-    m:
-    for(int i = 0; ;i++)
+m:
+    std::cout << "if want to enter in head than press h or in tail t\n";
+    std::string choose;
+    std::cin >> choose;
+    if (choose == "t")
     {
-        std::cout<<"Enter a value: ";
-        std::cin>>k;
-        std::cout<<"want to finish to inserting data than press 0\n";
-        if(k==0)
+        for (int i = 0;; i++)
         {
-            break;
+            std::cout << "Enter a value: ";
+            std::cin >> k;
+            std::cout << "want to finish to inserting data than press 0\n";
+            if (k == 0)
+            {
+                break;
+            }
+            insertAtTailValue(head, k);
         }
-        insertValue(head, k);
     }
-        std::cout<< "All data: ";
-        display(head);
-    
-    std::cout<<"\nWant to add data than press 1\n want to exit program than press 0\n";
+    else
+    {
+        for (int i = 0;; i++)
+        {
+            std::cout << "Enter a value: ";
+            std::cin >> k;
+            std::cout << "want to finish to inserting data than press 0\n";
+            if (k == 0)
+            {
+                break;
+            }
+            insertAtHeadValue(head, k);
+        }
+    }
+    std::cout << "All data: ";
+    display(head);
+
+    std::cout << "\nWant to add data than press 1\n want to exit program than press 0\n";
     int input;
-    std::cout<<"Input a choose:";
-    std::cin>>input;
-    if(input)
+    std::cout << "Input a choose:";
+    std::cin >> input;
+    if (input)
     {
         goto m;
     }
