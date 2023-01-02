31ab15825fb72e9eb5b6a30dfc9a3b674084e538
KPorus <designerfardin2021@gmail.com>
2023-01-02 19:11:47 +0600

    binary search in array

---
+++ BinarySearch.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+int binarySearch(int arr[], int n, int key)
+{
+    int s = 0;
+    int e = n;
+
+    while (s<=e)
+    {
+        int mid = (s+e)/2;
+
+        if(arr[mid] == key)
+        {
+            return mid;
+        }
+        else if(arr[mid]>key)
+        {
+            e = mid-1;
+        }
+        else{
+            s = mid + 1;
+        }
+    }
+    return -1;
+    
+}
+
+int main()
+{
+    int n;
+    std::cout<<"Enter the size og array:";
+    std::cin>> n;
+    int arr[n];
+    for (int  i = 0; i < n; i++)
+    {
+        std::cin>>arr[i];
+    }
+
+    int key = 0 ;
+    std::cout<<"Enter the searching value : ";
+    std::cin>>key;
+
+    int result = binarySearch(arr,n,key);
+    std::cout<< "Given value index in a array :"<< result;
+    
+    return 0;
+}
