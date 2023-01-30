d5a1f9e8ab52d36156bd90c3e7357c05f5b8ecbc
KPorus <designerfardin2021@gmail.com>
2023-01-30 18:30:37 +0600

    2nd pattern matching

--- SubStringSearch.c
+++ SubStringSearch.c
@@ -1,68 +1,38 @@
 #include<stdio.h>
-int i , j;
-void pattern (char arr[], char arr2[], int n)
-{
-    int max = n - 5 + 1;
-    for(i = 1;i<=max;i++)
-    {
-        int flag = 1;
-        for(j = 1; j<=5 && flag == 1; j++)
-        {
-            if(arr[i + j -1] != arr2[j])
-            {
-                flag = 0;
-            }
-        }
-        if(flag==1)
-        {
-            printf("Success");
-        }
-    }
-    printf("Fail");
-
-}
+#include<stdlib.h>
 int main(int argc, char const *argv[])
 {
-    int n;
-    printf("Enter a size string ");
-    scanf("%d",&n);
-    char arr[n];
-    printf("Enter a string ");
-    scanf("%s",arr);
-    char arr2[5];
-    printf("Enter a sub string ");
-    scanf("%s",arr2);
-    pattern(arr, arr2 ,n);
+    char str[]="fardin is back";
+    char str1[]="is";
+    int i = 0,j;
+    int k,temp;
+    while(str[i] != '\0')
+    {
+        j=0;
+        if(str[i] == str1[j])
+        {
+            k=i;
+            temp = i+1;
+            while(str[i]==str1[j])
+            {
+                i++;
+                j++;
+            }
+            if(str1[j] == '\0')
+            {
+                printf("The substring is present in given string at position \n %d to %d",k,i);
+                exit(0);
+            }
+            else{
+                i = temp;
+                temp =0;
+            }
+        }
+        i++;
+    }
+    if(temp == 0)
+    {
+        printf("The substring is not present");
+    }
     return 0;
 }
-
-// #include <stdio.h>
-// int i, j;
-// void pattern(char arr[], char arr2[])
-// {
-//     int max = 10 - 5 + 1;
-//     for (i = 0; i < max; i++)
-//     {
-//         int flag = 1;
-//         for (j = 0; j < 5; j++)
-//         {
-//             if (arr[i + j] != arr2[j])
-//             {
-//                 break;
-//             }
-//         }
-//         if (flag == 1)
-//         {
-//             printf("Success ");
-//         }
-//     }
-// }
-// int main(int argc, char const *argv[])
-// {
-
-//     char arr[10] = "fardinkhan";
-
-//     char arr2[5] = "fahdi";
-//     pattern(arr, arr2);
-//     return 0;
-// }
