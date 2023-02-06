ac713f225609b935080d2e2e8753407e445b13df
KPorus <designerfardin2021@gmail.com>
2023-02-06 23:13:50 +0600

    string incertion update

--- ArrayMergeWithSort.c
+++ ArrayMergeWithSort.c
@@ -24,18 +24,6 @@ int main()
     int arr3[size + size2];
     sort(arr1, size);
     sort(arr2, size2);
-    for (int i = 0; i < size - 1; i++)
-    {
-        for (int j = i + 1; j < size; j++)
-        {
-            if (arr1[j] < arr1[i])
-            {
-                int temp = arr1[i];
-                arr1[i] = arr1[j];
-                arr1[j] = temp;
-            }
-        }
-    }
 
     while (k < 15)
     {
--- StringInsertion.c
+++ StringInsertion.c
@@ -6,29 +6,61 @@ int length(char arr[])
     int count = 0;
     while (arr[i] != '\0')
     {
-        if (arr[i] == ' ')
-        {
-        }
-        else
-        {
-            count++;
-        }
         i++;
+        count++;
     }
     return count;
 }
 int main()
 {
     char str1[20] = "fardin is back";
-    char str2[20] = "anika";
-    int index = 14;
+    char str2[20] = "is";
+    int index = 12;
     int i, j;
 
-    for (j = 0; str2[j] != '\0'; j++)
+    int k, temp;
+    char str3[20] = "anika";
+    while (str1[i] != '\0')
     {
-        for (i = length(str1); i >= index; i--){
-            str1[i + 1] = str1[i];}
-        str1[index] = str2[j];
+        j = 0;
+        if (str1[i] == str2[j])
+        {
+            k = i;
+            int h;
+            temp = i + 1;
+            while (str1[i] == str2[j])
+            {
+                i++;
+                j++;
+            }
+            if (str2[j] == '\0')
+            {
+                printf("The substring is present in given string at position \n %d to %d\n", k, i);
+                index = k;
+
+                goto m;
+            }
+            else
+            {
+                i = temp;
+                temp = 0;
+            }
+        }
+        i++;
+    }
+    if (temp == 0)
+    {
+        printf("The substring is not present");
+        return 0;
+    }
+m:
+    for (j = 0; str3[j] != '\0'; j++)
+    {
+        for (i = length(str1); i >= index; i--)
+        {
+            str1[i + 1] = str1[i];
+        }
+        str1[index] = str3[j];
         index++;
     }
 
